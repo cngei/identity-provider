ea4f4871b0b05964d745e6ca5b53ceab6e92c1ab
Paolo Campanelli <paolo.campanelli@cngei.it>
2020-10-23 22:09:54 +0200

    feat: import realm definition

--- import/index.js
+++ import/index.js
@@ -2,6 +2,7 @@ const KeycloakAdminClient = require('keycloak-admin').default;
 
 // SELECT * FROM edp.fe_users;
 const oldUsers = require('./dump.json').fe_users
+const realmData = require('./realm-export.json')
 
 const KC_HOST = 'http://localhost:8080'
 const KC_USER = 'admin'
@@ -11,14 +12,29 @@ const kcAdminClient = new KeycloakAdminClient({
     baseUrl: `${KC_HOST}/auth`,
 });
 
-const importUsers = async () => {
-    await kcAdminClient.auth({
-        username: KC_USER,
-        password: KC_PASSWORD,
-        grantType: 'password',
-        clientId: 'admin-cli'
-    });
+const authenticate = async () => {
+    try {
+        await kcAdminClient.auth({
+            username: KC_USER,
+            password: KC_PASSWORD,
+            grantType: 'password',
+            clientId: 'admin-cli'
+        });
+    } catch (e) {
+        console.log(e)
+    }
+}
 
+const initializeRealm = async () => {
+    try {
+        await kcAdminClient.realms.create(realmData)
+        console.log("Realm CNGEI created")
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const importUsers = async () => {
     // Delete existing users, just in case
     const users = (await kcAdminClient.users.find({ max: 9999, realm: 'cngei' })).filter(x => x.username !== 'admin')
     for (let user of users) {
@@ -46,11 +62,19 @@ const importUsers = async () => {
                 emailVerified: true
             })
         } catch (e) {
-            // There are users with the same email, what do we do?
             console.log(user.email, user.username)
-            // console.log(e)
+            console.log(e.response.data)
         }
     }
 }
 
-importUsers()
+const importSezioni = async () => {
+    
+}
+
+(async () => {
+    await authenticate()
+    await initializeRealm();
+    await importUsers();
+    await importSezioni();
+})()
